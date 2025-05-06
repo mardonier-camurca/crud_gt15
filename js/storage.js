545d006e644797afa46b7286a3cf18cb7ae474de
mardonier <mardonier@hotmail.com>
2025-05-06 15:05:31 -0300

    created crud with js

---
+++ js/storage.js
@@ -0,0 +1,8 @@
+export function obterUsuarios() {
+    const dados = localStorage.getItem('usuarios');
+    return dados ? JSON.parse(dados) : [];
+}
+
+export function salvarUsuarios(usuarios) {
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+}
